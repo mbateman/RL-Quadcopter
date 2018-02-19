4755ec7caeb33895220ec1d88826f3e950992dff
Michael Bateman <michael.bateman.com@gmail.com>
2018-02-19 13:38:28 +0000

    changed to use policy gradients

--- quad_controller_rl/src/quad_controller_rl/agents/__init__.py
+++ quad_controller_rl/src/quad_controller_rl/agents/__init__.py
@@ -1,3 +1,3 @@
 from quad_controller_rl.agents.base_agent import BaseAgent
 from quad_controller_rl.agents.policy_search import RandomPolicySearch
-from quad_controller_rl.agents.ddpg_agent import DDPG
+from quad_controller_rl.agents.policy_gradients import DDPG
--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -1,47 +1,35 @@
-"""Policy search agent."""
+"""Deep Deterministec Policy Gradients (DDPG) reinforcement learning agent."""
+import numpy as np
 import os
 import pandas as pd
-import numpy as np
 
-from quad_controller_rl import util
 from quad_controller_rl.agents.base_agent import BaseAgent
-
 from quad_controller_rl.agents.actor import Actor
 from quad_controller_rl.agents.critic import Critic
-# from quad_controller_rl.agents.memory import Memory
 from quad_controller_rl.agents.replay_buffer import ReplayBuffer
 from quad_controller_rl.agents.ou_noise import OUNoise
+from quad_controller_rl import util
 
 
 class DDPG(BaseAgent):
-    def __init__(self, task):
-        # Task (environment) information
-        self.task = task
+    """Reinforcement Learning agent that learns using DDPG. """
 
-        # constrain size
-        self.state_size = 3  # position only (x,y,z)
-        self.action_size = 3  # force only (x,y,z)
+    def __init__(self, task):
+
+        # Initialize environment variables
+        self.task = task
+        self.state_size = 3  # position only
+        self.state_range = self.task.observation_space.high - self.task.observation_space.low
+        self.action_size = 3  # state only
+        self.action_range = self.task.action_space.high - self.task.action_space.low
 
         # Actor (Policy) Model
         self.action_low = self.task.action_space.low[0:3]
         self.action_high = self.task.action_space.high[0:3]
-        # debugger
-        # import pdb; pdb.set_trace()
-
-        # Score tracker and learning parameters
-        self.best_w = None
-        self.best_score = -np.inf
-        self.noise_scale = 0.1
-
-        # Save episode stats
-        self.stats_filename = os.path.join(util.get_param('out'), "stats_{}.csv".format(util.get_timestamp()))
-        self.stats_columns = ['episode', 'total_reward']  # specify columns to save
-        self.episode_num = 1
-        # [debug]
-        print("Saving stats {} to {}".format(self.stats_columns, self.stats_filename))
-
-        self.actor_local = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
-        self.actor_target = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
+        self.actor_local = Actor(self.state_size, self.action_size, self.action_low,
+                                 self.action_high)
+        self.actor_target = Actor(self.state_size, self.action_size, self.action_low,
+                                  self.action_high)
 
         # Critic (Value) Model
         self.critic_local = Critic(self.state_size, self.action_size)
@@ -60,8 +48,16 @@ class DDPG(BaseAgent):
         self.memory = ReplayBuffer(self.buffer_size)
 
         # Algorithm parameters
-        self.gamma = 0.99  # discount factor
-        self.tau = 0.001  # for soft update of target parameters
+        self.gamma = 0.99
+        self.tau = 0.001
+
+        # Save episode stats
+        self.stats_filename = os.path.join(
+            util.get_param('out'),
+            "stats_{}.csv".format(util.get_timestamp()))  # path to CSV file
+        self.stats_columns = ['episode', 'total_reward']  # specify columns to save
+        self.episode_num = 1
+        print("Saving stats {} to {}".format(self.stats_columns, self.stats_filename))  # [debug]
 
         # Episode variables
         self.reset_episode_vars()
@@ -70,46 +66,67 @@ class DDPG(BaseAgent):
         self.last_state = None
         self.last_action = None
         self.total_reward = 0.0
-        self.count = 0
+        self.actor_loss = 0.0
+        self.critic_loss = 0.0
+
+    def preprocess_state(self, state):
+        """Reduce sate vector to relevant dimensions"""
+        return state[0:3]  # position only
+
+    def postprocess_action(self, action):
+        """Return complete action vector."""
+        complete_action = np.zeros(self.task.action_space.shape)  # shape: (6,)
+        action[-1, 2] = np.abs(action[-1, 2])
+        complete_action[0:3] = action  # linear force only
+        return complete_action
 
     def step(self, state, reward, done):
         # Reduce state vector
         state = self.preprocess_state(state)
+
+        # Transform state vector
+        # state = (state - self.task.observation_space.low[0:3]) / (self.task.observation_space.high[0:3] - self.task.observation_space.low[0:3])  # scale to [0.0, 1.0]
+        # state = state.reshape(1, -1)  # convert to row vector
+        # print("normalized_state: {}".format(state))
 
         # Choose an action
         action = self.act(state)
 
         # Save experience / reward
         if self.last_state is not None and self.last_action is not None:
-            self.memory.add(self.last_state, self.last_action, reward, state, done)
+            self.memory.add(self.last_state, self.last_action, reward,
+                            state, done)
             self.total_reward += reward
-            self.count += 1
 
-        # Learn, if at end of episode
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > self.batch_size:
+            experiences = self.memory.sample(self.batch_size)
+            self.learn(experiences)
+
         if done:
-            # Learn, if enough samples are available in memory
-            if len(self.memory) > self.batch_size:
-                experiences = self.memory.sample(self.batch_size)
-                self.learn(experiences)
             # Write episode stats
             self.write_stats([self.episode_num, self.total_reward])
             self.episode_num += 1
+            print("Total reward: {0:.4f}... Actor loss: {1:.4f}... Critic loss: {2:.4f}...".format(
+                self.total_reward, self.actor_loss[0], self.critic_loss))
             self.reset_episode_vars()
 
         self.last_state = state
         self.last_action = action
-        # Return complete action vector
         return self.postprocess_action(action)
 
     def act(self, states):
         """Returns actions for given state(s) as per current policy."""
         states = np.reshape(states, [-1, self.state_size])
         actions = self.actor_local.model.predict(states)
+        self.actions = actions
         return actions + self.noise.sample()  # add some noise for exploration
 
     def learn(self, experiences):
-        """Update policy and value parameters using given batch of experience tuples."""
-        # Convert experience tuples to separate arrays for each element (states, actions, rewards, etc.)
+        """Update policy and value parameters using given batch of experience
+        tuples."""
+        # Convert experience tuplies to separate arrays for each element ( states,
+        # actions, rewards, etc.)
         states = np.vstack([e.state for e in experiences if e is not None])
         actions = np.array([e.action for e in experiences if e is not None]).astype(np.float32).reshape(-1,
                                                                                                         self.action_size)
@@ -118,26 +135,24 @@ class DDPG(BaseAgent):
         next_states = np.vstack([e.next_state for e in experiences if e is not None])
 
         # Get predicted next-state actions and Q values from target models
-        #     Q_targets_next = critic_target(next_state, actor_target(next_state))
+        # Q_targets_next = critic_target(next_state, actor_target(next_state))
         actions_next = self.actor_target.model.predict_on_batch(next_states)
-        Q_targets_next = self.critic_target.model.predict_on_batch([next_states, actions_next])
+        Q_targets_next = self.critic_target.model.predict_on_batch([
+            next_states, actions_next])
 
         # Compute Q targets for current states and train critic model (local)
         Q_targets = rewards + self.gamma * Q_targets_next * (1 - dones)
-        self.critic_local.model.train_on_batch(x=[states, actions], y=Q_targets)
+        self.critic_loss = self.critic_local.model.train_on_batch(x=[states, actions], y=Q_targets)
 
         # Train actor model (local)
-        action_gradients = np.reshape(self.critic_local.get_action_gradients([states, actions, 0]),
-                                      (-1, self.action_size))
-        self.actor_local.train_fn([states, action_gradients, 1])  # custom training function
+        action_gradients = np.reshape(self.critic_local.get_action_gradients(
+            [states, actions, 0]), (-1, self.action_size))
+        self.action_gradients = action_gradients
+        self.actor_loss = self.actor_local.train_fn([states, action_gradients, 1])  # custom train fn
 
         # Soft-update target models
         self.soft_update(self.critic_local.model, self.critic_target.model)
         self.soft_update(self.actor_local.model, self.actor_target.model)
-
-        # print("DDPG.learn(): t = {:4d}, score = {:7.3f} (best = {:7.3f}), noise_scale = {}".format(
-            # self.count, score, self.best_score, self.noise_scale))  # [debug]
-        # print(self.w)  # [debug: policy parameters]
 
     def soft_update(self, local_model, target_model):
         """Soft update model parameters."""
@@ -146,16 +161,6 @@ class DDPG(BaseAgent):
 
         new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
         target_model.set_weights(new_weights)
-
-    def preprocess_state(self, state):
-        """Reduce state vector to relevant dimensions."""
-        return state[0:3]  # position only
-
-    def postprocess_action(self, action):
-        """Return complete action vector."""
-        complete_action = np.zeros(self.task.action_space.shape)  # shape: (6,)
-        complete_action[0:3] = action  # linear force only
-        return complete_action
 
     def write_stats(self, stats):
         """Write single episode stats to CSV file."""
